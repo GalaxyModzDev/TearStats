c84ec727207a59c104f54a08568ed1b980344e4d
Galaxy Modz <66964223+GalaxyModzYT@users.noreply.github.com>
2024-01-02 19:44:54 -0700

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,32 @@
+--This script will run on all levels when mod is active.
+--Modding documentation: http://teardowngame.com/modding
+--API reference: http://teardowngame.com/modding/api.html
+
+local startTime = 0
+
+function init()
+    distance = 0
+    startTime = GetTime()
+    lastPos = GetPlayerTransform().pos
+    end
+    
+function tick(dt)
+    distance = distance + VecLength(VecSub(GetPlayerTransform().pos,lastPos))
+    lastPos = GetPlayerTransform().pos
+    DebugWatch("Distance Travelled Since Level Load", math.floor(distance*10)/10 .. " Voxels")
+    ticktwo(dt)
+end
+
+function ticktwo(dt)
+    local elapsedTime = GetTime() - startTime
+    DebugWatch("Time Since Loaded", math.floor(elapsedTime * 10) / 10 .. " Seconds")
+end
+
+function update(dt)
+end
+
+
+function draw(dt)
+end
+
+
